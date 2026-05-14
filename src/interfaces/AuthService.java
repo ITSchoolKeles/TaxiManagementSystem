0917592e1c4444b44dbd1b13a3a746809cd03ed6
akbarabdurahimov481-hue <akbarabdurahimov481@gmail.com>
2026-05-14 19:06:54 +0500

    adminDashboard menu is displayed

--- src/interfaces/AuthService.java
+++ src/interfaces/AuthService.java
@@ -2,7 +2,10 @@ package interfaces;
 
 import classes.User;
 
+import java.util.ArrayList;
+
 public interface AuthService {
     User login(String phoneNumber,String password);
     User register(User user);
+    ArrayList<User> getUsers();
 }
